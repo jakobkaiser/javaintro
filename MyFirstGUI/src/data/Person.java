255328e5ba4f6c4c6d99d2514b5da313bb8ce7b4
jakobkaiser <jakobkaiser@AMS-GYM-EDV1-14.gym.ams.local>
2018-05-15 15:46:41 +0200

    15.05.2018

--- MyFirstGUI/src/data/Person.java
+++ MyFirstGUI/src/data/Person.java
@@ -1,12 +1,16 @@
 package data;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 public class Person {
 
@@ -15,32 +19,64 @@ public class Person {
 	public static ArrayList<Person> getPersonenListe() {
 		return personenListe;
 	}
+
 	public static void write2JSON() {
 		try {
 			Writer writer = new FileWriter("C:\\temp\\output.json");
 			Gson gson = new GsonBuilder().serializeNulls().create();
 			gson.toJson(Person.getPersonenListe(), writer);
 			
+						
+
 			writer.flush();
 			writer.close();
-			
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
-			
+
 		}
 	}
+
+	public static void readFromJSON(){
+		try {
+			Writer writer = new FileWriter("C:\\temp\\output.json");
+			Gson gson = new GsonBuilder().serializeNulls().create();
+
+			ArrayList<Person> personen = new ArrayList<>();
+			//
+			gson.toJson(personen, writer);
+			//
+			writer.flush();
+			writer.close();
+			//
+			Reader r = new FileReader("C:\\temp\\output.json");
+			ArrayList<Person> personenRead = new ArrayList<>();
+			Type listType = new TypeToken<ArrayList<Person>>() {
+			}.getType();
+			personenRead = gson.fromJson(r, listType);
+			for (Person p2 : personenRead)
+				System.out.println(p2);
+			System.out.println("---------------");
+			for (Person p2 : personenRead)
+				System.out.println(p2);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+				}
 
 	public Person() {
 
 	}
 
-		// rechtsclick -> source -> getter setter 
+	// rechtsclick -> source -> getter setter
 	private String vorname;
 	private String nachname;
 	private String strasse;
-    private String ort;
-    private int plz;
-    
+	private String ort;
+	private int plz;
+
 	public String getVorname() {
 		return vorname;
 	}
@@ -65,7 +101,6 @@ public class Person {
 		this.strasse = strasse;
 	}
 
-
 	public int getPlz() {
 		return plz;
 	}
@@ -73,7 +108,6 @@ public class Person {
 	public void setPlz(int plz) {
 		this.plz = plz;
 	}
-
 
 	public String getOrt() {
 		return ort;
@@ -92,17 +126,17 @@ public class Person {
 	public String toString() {
 		return getVorname() + " " + getNachname() + ", " + getStrasse() + ", " + getOrt() + ",  " + "Plz: " + getPlz();
 	}
-	
+
 }
 // java.com
-//java runterladen JAVA8 oder so
-//eclipse download packages  eclipse.org
-//windowbuilder bei eclipse  4.7 Oxygen 
+// java runterladen JAVA8 oder so
+// eclipse download packages eclipse.org
+// windowbuilder bei eclipse 4.7 Oxygen
 // HELP install new softwear enter drücken
 // gson jar download V2.8.1 Jar datei
-//new other swt/jface java project gui2 finish
-//source package gui und data package
-// in gui  new other application window GUI2
+// new other swt/jface java project gui2 finish
+// source package gui und data package
+// in gui new other application window GUI2
 //// variablen name vorname
 // in data new class Person
 //// private String vorname;
@@ -111,7 +145,20 @@ public class Person {
 // Private static arrayliste<Person> personenListe = new Arraylist
 // nur getter method
 // in gui2 Person p = new Person();
-//p.setNachname(getNachname().getText();
-//gson datei in src ordner ziehen . rechte maustast build path add to build path
-//public static void write2JSON
+// p.setNachname(getNachname().getText();
+// gson datei in src ordner ziehen . rechte maustast build path add to build
+// path
+// public static void write2JSON
 ///
+// ArrayList<Person>
+// Reader r = new FileReader(_.._);
+// personenRead = gson.fromJson(json, idk dog Arraylist<Person>>(){}.getType();
+// AL<Person> personenRead=new AL<>();
+// for (P p2 : personenR)
+// syso p2
+// public static void readFromJSON(){
+// personenListe = readFromJSON
+// }
+// button machen aus Datei lesen
+//private List GuiListe
+//get GuiListe()
--- MyFirstGUI/src/gui/JSONTest.java
+++ MyFirstGUI/src/gui/JSONTest.java
@@ -1,11 +1,16 @@
 package gui;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.lang.reflect.Type;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import data.Person;
 
@@ -15,23 +20,49 @@ public class JSONTest {
 		try {
 			Writer writer = new FileWriter("C:\\temp\\output.json");
 			Gson gson = new GsonBuilder().serializeNulls().create();
-//			gson.toJson("Hallo", writer);
-//			gson.toJson(12345, writer);
-//            gson.toJson("vorname", writer);
-//            gson.toJson("plz:", writer);
-            
-			Person p= new Person();
+
+			ArrayList<Person> personen = new ArrayList<>();
+			Person p = new Person();
 			p.setVorname("Cementer");
 			p.setNachname("Fenster Runter");
 			p.setPlz(1180);
 			p.setStrasse("Haselnussstrasse");
 			p.setOrt("Albanien");
+
+			personen.add(p);
+			p = new Person();
+			p.setVorname("Cementer1");
+			p.setNachname("Fenster Runter");
+			p.setPlz(1180);
+			p.setStrasse("Haselnussstrasse");
+			p.setOrt("Albanien");
+			
+			personen.add(p);
+			p = new Person();
+			p.setVorname("Cementer2");
+			p.setNachname("Fenster Runter");
+			p.setPlz(1180);
+			p.setStrasse("Haselnussstrasse");
+			p.setOrt("Albanien");
+			personen.add(p);
+
 			//
-			gson.toJson(p, writer);
+			gson.toJson(personen, writer);
 			//
 			writer.flush();
 			writer.close();
 			//
+			Reader r = new FileReader("C:\\temp\\output.json");
+			ArrayList<Person> personenRead = new ArrayList<>();
+			Type listType = new TypeToken<ArrayList<Person>>() {
+			}.getType();
+			personenRead = gson.fromJson(r, listType);
+			for (Person p2 : personenRead)
+				System.out.println(p2);
+			System.out.println("---------------");
+			for (Person p2 : personenRead)
+				System.out.println(p2);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
